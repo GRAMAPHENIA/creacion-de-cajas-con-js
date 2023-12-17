884ae8aeaca37211942e7310846b291c510d737b
gramaphenia <dicoratojuanpablo@gmail.com>
2023-12-17 07:57:31 -0300

    Inicio

---
+++ README.md
@@ -0,0 +1,11 @@
+# CREAR CAJAS CON JS
+
+### ARCHIVOS
+
+- Creacion de archivo `index.html`
+- Creacion de archivo `styles.css`
+- Creacion de archivo `script.js`
+
+---
+
+Una vez creados los archivos pasamos escribir las estructuras basicas de cada uno. Para el `index.html` un sistema de cajas con sus respectivos `id's` los cuales vamos a llamar desde el `js` y por los cuales vamos a agregar el contenido.
---
+++ script.js
@@ -0,0 +1,65 @@
+// Crear un nuevo elemento div
+let cajaUno = document.createElement("div");
+
+// Agregar una clase al nuevo div
+cajaUno.classList.add("segmento");
+
+// Contenido del div
+cajaUno.innerHTML = "Caja agregada con JavaScript 🟡";
+
+// Obtener el contenedor donde se agregar el nuevo div
+let contenedor = document.getElementById("caja-uno");
+
+// Agregar el nuevo div al contenedor
+contenedor.appendChild(cajaUno);
+
+// A solas uno...
+
+let cajaDos = document.createElement("div")
+
+cajaDos.classList.add("segmento")
+
+cajaDos.innerHTML = "Caja agrgada con JavaSacript 🟡"
+
+let contenedorDos = document.getElementById('caja-dos')
+
+contenedorDos.appendChild(cajaDos)
+
+
+// A solas dos...
+
+let cajaTres = document.createElement("div");
+
+cajaTres.classList.add("segmento");
+
+cajaTres.innerHTML = "Caja agregada con JavaScript 🟡";
+
+let contenedorTres = document.getElementById("caja-tres");
+
+contenedorTres.appendChild(cajaTres);
+
+// A solas tres...
+
+let cajaCuatro = document.createElement("div");
+
+cajaCuatro.classList.add("segmento");
+
+cajaCuatro.innerHTML = "Caja agregada con JavaScript 🟡";
+
+let contenedorCuatro = document.getElementById("caja-cuatro")
+
+contenedorCuatro.appendChild(cajaCuatro)
+
+// A solas cuatro...
+
+let cajaCinco = document.createElement("div")
+
+cajaCinco.classList.add("segmento")
+
+cajaCinco.innerHTML = "Caja agregada con JavasCript 🟡"
+
+let contenedorCinco = document.getElementById("caja-cinco")
+
+contenedorCinco.appendChild(cajaCinco)
+
+
